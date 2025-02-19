9f6b0fc940d5ed8876b580afa0a3e67f8b885837
Paul Grimes <paul.grimes@amd.com>
2025-02-19 07:50:12 -0800

    need to stop tracking

--- MdeModulePkg/Bus/Usb/UsbMassStorageDxe/UsbMassBot.h
+++ MdeModulePkg/Bus/Usb/UsbMassStorageDxe/UsbMassBot.h
@@ -1,9 +1,5 @@
-/** @file
-
-  Copyright (C) 2023-2025 Advanced Micro Devices, Inc. All rights reserved.<BR>
-
-  SPDX-License-Identifier: BSD-2-Clause-Patent
-
+/**
+   Copyright (C) 2023 Advanced Micro Devices, Inc. All rights reserved.
 **/
 
 /** @file
